0242d60f8c988f6dcf9828edb4269b6e59991c11
Ravi Patel <ravipatel1713@gmail.com>
2020-11-03 19:04:45 -0600

    Implemented method for creating new particles in simulator.

--- src/visualizer/simulator.cc
+++ src/visualizer/simulator.cc
@@ -31,27 +31,17 @@ void Simulator::Clear() {
 
 void Simulator::AddParticles(const glm::vec2 &pos) {
   // checks bounds
+  if (pos.x > top_left_corner_.x &&
+      pos.x < top_left_corner_.x + simulator_size_ &&
+      pos.y > top_left_corner_.y &&
+      pos.y < top_left_corner_.y + simulator_size_){
+    particles.emplace_back(glm::vec2(pos.x, pos.y),
+                           glm::vec2(5, 5));
+  }
 }
 void Simulator::UpdateParticlePosition() {
   for(int i = 0; i < particles.size(); ++i){
-    if (particles[i].pos_.x - 7 <= top_left_corner_.x &&
-        particles[i].vel_.x < 0){
-      particles[i].vel_.x = -1 * particles[i].vel_.x;
-    }
-    else if (particles[i].pos_.x + 7 >= top_left_corner_.x + simulator_size_&&
-        particles[i].vel_.x > 0){
-      particles[i].vel_.x = -1 * particles[i].vel_.x;
-    }
-
-    if (particles[i].pos_.y - 7 <= top_left_corner_.y &&
-        particles[i].vel_.y < 0){
-      particles[i].vel_.y = -1 * particles[i].vel_.y;
-    }
-    else if (particles[i].pos_.y + 7 >= top_left_corner_.y + simulator_size_&&
-        particles[i].vel_.y > 0){
-      particles[i].vel_.y = -1 * particles[i].vel_.y;
-    }
-
+    
     particles[i].pos_ +=  particles[i].vel_;
   }
 }
